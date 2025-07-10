9cb95c714771f9d84e1c520e2a89255af636f9bd
Daryl <daryl@Isaacs-MacBook-Pro.local>
2025-07-10 13:59:29 -0700

    updated favicon and title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -45,8 +45,8 @@ export const Route = createRootRouteWithContext<{
       },
       ...seo({
         title:
-          'TanStack Start | Type-Safe, Client-First, Full-Stack React Framework',
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+          'PiggyBanx Command Center',
+        description: `Platform for managing PiggyBanx's operations`,
       }),
     ],
     links: [
@@ -54,19 +54,19 @@ export const Route = createRootRouteWithContext<{
       {
         rel: 'apple-touch-icon',
         sizes: '180x180',
-        href: '/apple-touch-icon.png',
+        href: '/Logo-Bolt-White-Circle.png',
       },
       {
         rel: 'icon',
         type: 'image/png',
         sizes: '32x32',
-        href: '/favicon-32x32.png',
+        href: '/Logo-Bolt-White-Circle.png',
       },
       {
         rel: 'icon',
         type: 'image/png',
         sizes: '16x16',
-        href: '/favicon-16x16.png',
+        href: '/Logo-Bolt-White-Circle.png',
       },
       { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
       { rel: 'icon', href: '/favicon.ico' },
